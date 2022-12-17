7faf7f8506eaa19f4ccc7b4857e334c0e13e16e6
karmenis <andreas_karmenis@hotmail.com>
2022-12-17 14:20:23 +0200

    front-end - app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,26 @@
 import { Component } from '@angular/core';
-
+import { RequestData } from './RequestData'; 
+import { RequestDataService } from './request-data.service'; 
+ 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  requestData: RequestData;
+  list: requestData.list;
+
+  sendListToBeSorted() {
+
+      this.requestDataService
+      .requestToSortList(this.requestData)
+      .subscribe((response: UseRequestData) => {
+          console.log(response);
+      });
+
+  }
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -9,6 +10,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    ReactiveFormsModule 
   ],
   providers: [],
   bootstrap: [AppComponent]
